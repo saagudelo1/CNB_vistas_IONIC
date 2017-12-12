5cf40d1300a55c9a0a82b65b04fea905c6ab9e7a
SantyAgu <32776322+SantyAgu@users.noreply.github.com>
2017-12-12 08:41:27 -0500

    Splash, Icon, fuentes y cambios en las rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
--- src/components/menu-operador/menu-operador.ts
+++ src/components/menu-operador/menu-operador.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './menu-operador.html',
 })
 export class MenuOperadorComponent implements OnInit {
-  public semaforo:string= "/assets/semaforo/semaforo_rojo.png";
+  public semaforo:string= "assets/semaforo/semaforo_rojo.png";
   public ventas;
   public mostrarr;
   colores=["semaforo_rojo.png","semaforo_amarillo.png","semaforo_verde.png","semaforo_dorado.png"];
@@ -19,7 +19,7 @@ export class MenuOperadorComponent implements OnInit {
       let num = Math.floor(Math.random() * (4 - 0)) + 0;
       this.ventas= (num+1) +"00"; 
       this.mostrarr =this.mostrar();   
-      this.semaforo = "/assets/semaforo/"+this.colores[(num)];     
+      this.semaforo = "assets/semaforo/"+this.colores[(num)];     
     }, 2000);
   }
   mostrar(){

0a921ef91d0f6c172bcb373ed8f6cbe5578b8054
DIR\cristian.c.meneses <cristian.c.meneses@accenture.com>
2017-12-13 15:02:47 -0500

    varios cambios

--- src/components/menu-operador/menu-operador.ts
+++ src/components/menu-operador/menu-operador.ts
@@ -5,9 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './menu-operador.html',
 })
 export class MenuOperadorComponent implements OnInit {
-  public semaforo:string= "assets/semaforo/semaforo_rojo.png";
-  public ventas;
-  public mostrarr;
+  public semaforo:string= "assets/semaforo/semaforo_verde.png";
+  public Vnombre= ["Andrea Agudelo","Patricia Ocampa","Luis Fonsi","Santiago Mendez"];
+  public nombre;
+  public cupo;
+  public mostrar:boolean= false;
   public barra:number=0;
   public barraVerde;
   public barraAmarillo;
@@ -17,52 +19,39 @@ export class MenuOperadorComponent implements OnInit {
   constructor() { }
   
   ngOnInit() {
+    let num = Math.floor(Math.random() * (4 - 0)) + 0;
+    this.nombre = this.Vnombre[num];
+    console.log(this.nombre)
     setInterval(()=>{    
       this.barra++;      
-      this.ventas = (100-this.barra); 
-      this.llenarBarra1();
-      
-
+      this.cupo = (100-this.barra); 
+      this.llenarBarra1();   
     },300);
     
-    setInterval(()=>{
-      let num = Math.floor(Math.random() * (4 - 0)) + 0;
-      
-      this.mostrarr =this.mostrar();   
-      this.semaforo = "assets/semaforo/"+this.colores[(num)];     
-    }, 2000);
+ 
   }
   llenarBarra1(){    
     if (this.barra <= 50) {
       this.barraVerde=this.barra+"%";
+      this.semaforo = "assets/semaforo/"+this.colores[2];
     }else{
       if (this.barra <= 80) {
         this.barraAmarillo=(this.barra-50)+"%";
+        this.semaforo = "assets/semaforo/"+this.colores[1];
       } else {
         if (this.barra <= 100) {
           this.barraRojo=(this.barra-80)+"%";
+          this.semaforo = "assets/semaforo/"+this.colores[0];
         } else {
-          this.barra = 0;
-          this.barraRojo = 0;
-          this.barraAmarillo = 0;
-          this.barraVerde = 0;
-          
-          
+          this.barra = this.barraRojo = 0;          
+          this.barraAmarillo = this.barraVerde = 0;
         }        
       }      
     }
-
-  };
-  
-  mostrar(){
-    if(this.ventas=="100" ||this.ventas=="200")
-      return false;
-    else
-     return true;
   };
 
+  mosBarra(val:boolean){    
+      this.mostrar=val;          
+  }
   
-  
-  
-
 }

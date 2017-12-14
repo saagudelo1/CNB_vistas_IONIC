a4bfc59c4982c410cbe06a8bb2d2a8c69d3b3413
osoriojuan77 <34067019+osoriojuan77@users.noreply.github.com>
2017-12-14 08:35:56 -0500

    imprimir

---
+++ src/components/imprimir/Model/Deposito.ts
@@ -0,0 +1,20 @@
+export class Deposito{
+    
+        constructor(
+            public Tipo_Op: string,
+            public Nombre: string,
+            public Tipo_Cuenta: string,
+            public Num_Cuenta: number,
+            public Monto: number,
+            public Num_Compro:number,
+            public Num_transa: number,           
+    
+        )
+        {
+    
+    
+        }
+        
+    
+    
+    }
--- src/components/imprimir/imprimir.ts
+++ src/components/imprimir/imprimir.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import{Deposito} from './Model/Deposito';
 
 @Component({
   selector: 'imprimir',
   templateUrl: 'imprimir.html'
 })
 export class ImprimirComponent {
+  public deposito:Deposito;
 
-  text: string;
+ 
 
   constructor() {
+
+    this.deposito = new Deposito( "Deposito", "Jairo Meneses", "Ahorros", 123456789, 10000, 123456, 123);
     
   }
 

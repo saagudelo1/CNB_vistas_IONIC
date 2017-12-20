2b41f40ee69b151e9bb2d1ad2b21bea7df139e0c
osoriojuan77 <34067019+osoriojuan77@users.noreply.github.com>
2017-12-20 07:50:15 -0500

    cambios imprimir

--- src/components/imprimir/imprimir.ts
+++ src/components/imprimir/imprimir.ts
@@ -8,8 +8,8 @@ import { DateTime } from "ionic-angular/components/datetime/datetime";
 })
 export class ImprimirComponent {
   public deposito:Deposito;
-  public col1 = ["Numero de Transaccion:"," Fecha:","Hora:","Nombre del Corresponsal no Bancario:","Numero del Punto:","Nombre de la Transaccion:","Numero de la cuenta Destino:",
-"Nombre del Cliente:", "Monto del Deposito:", "Efectivo Entregado:","Cambio:"];
+  public col1 = ["Número de Documento:"," Fecha y Hora:","Nombre Corresponsal:","Concepto:","Monto:","Deposito en efectivo:",
+"Monto Entregado:","Cambio:"];
   public col2;
   
 
@@ -18,8 +18,7 @@ export class ImprimirComponent {
   constructor() {
 
     this.deposito = new Deposito( "0001", new Date("dd/MM/yyyy").toString() , "Ahorros", 123456789, 10000, 123456, 123);
-    this.col2 = ["0001", (new Date().getDate()) + "/" +(new Date().getMonth()+1) + "/" +(new Date().getFullYear()) , new Date().toLocaleTimeString(), "OLA 1", 1, "Deposito", 123567890,"Pedro Palacio", "$9,000.00", "$10,000.00","$1,000.00"  ];    
-
+    this.col2 = ["1234567890", (new Date().getDate()) + "/" +(new Date().getMonth()+1) + "/" +(new Date().getFullYear()) +" "+ new Date().toLocaleTimeString(), "OLA 1","Transferencia" , "$9,000.00", "$9,000.00","$10,000.00","$1,000.00"  ];  
        
 
   }

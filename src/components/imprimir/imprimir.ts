3008bf0e7e29228f5ce6318c7bfeec71789ed908
osoriojuan77 <34067019+osoriojuan77@users.noreply.github.com>
2017-12-21 16:55:03 -0500

    Redireccion a imprimir

--- src/components/imprimir/imprimir.ts
+++ src/components/imprimir/imprimir.ts
@@ -1,35 +1,48 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { DateTime } from "ionic-angular/components/datetime/datetime";
+import { registerModeConfigs } from "ionic-angular/config/mode-registry";
 
 @Component({
   selector: "imprimir",
   templateUrl: "imprimir.html"
 })
-export class ImprimirComponent {
+export class ImprimirComponent implements OnInit {
   
-  public col1 = ["Número de Transacción:"," Fécha y Hora:","Nombre CNB:","Número CNB:","Concepto:","Número de Cuenta:",
-"Nombre del Cliente:","Monto Depositado:","Monto Entregado","Cambio"];
+  public col1;
   public col2; 
-
+  reimpresion:number;
   
   constructor() {
 
-    let Deposito:JSON = JSON.parse(localStorage.getItem("Deposito"));
-    console.log("Los datos del depósito son: ", Deposito);
-    
-    this.col2 = ["1234567890", (new Date().getDate()) + "/" +(new Date().getMonth()+1) + "/" +(new Date().getFullYear()) +" "+ new Date().toLocaleTimeString(), "OLA 1","Transferencia" , "$9,000.00", "$9,000.00","$10,000.00","$1,000.00"  ];  
-       
 
   }
   
-  public imprSelec (DatosImp)
-  {var ficha=document.getElementById(DatosImp);
-    var ventimp=window.open(' ','popimpr');
+  ngOnInit(){
+    let Deposito:JSON = JSON.parse(localStorage.getItem("Deposito"));
+    console.log("Los datos del depósito son: ", Deposito);
+    this.col1 = ["Número de Transacción:"," Fécha y Hora:","Nombre CNB:","Número CNB:","Concepto:","Número de Cuenta:",
+    "Nombre del Cliente:","Monto Depositado:","Monto Entregado:","Cambio:"];
+    this.col2 = [Deposito["numeroTransaccion"], Deposito["fechaHora"], Deposito["nombreCNB"] , Deposito["numeroCNB"], Deposito["concepto"], Deposito["numeroCuenta"], Deposito["nombreCliente"], Deposito["montoDepositado"], Deposito["montoEntregado"], Deposito["cambio"]]
+       
+    this.reimpresion = 0;
+    setTimeout(this.imprSelec,1000)
+
+    
+  }
+  public imprSelec ()
+  {var ficha=document.getElementById("DatosImp");
+    var ventimp = window.open(' ','popimpr');
+    if (ventimp == undefined){
+      alert("Por favor habilite las ventanas emergentes")
+    }
+    else
+    {
     ventimp.document.write(ficha.innerHTML);
     ventimp.print();
     ventimp.close();
+    this.reimpresion += 1;
+    }
   }
-  
-  
+ 
 }
 
